6cb402966348944e139c381a272bca468cc4061e
lcw3176 <lcw3176@naver.com>
2023-01-03 16:05:53 +0900

    feat(NoticeRepository): 가장 최신글 탐색 함수 추가

--- src/main/java/com/joebrooks/mapshotapi/repository/notice/NoticeRepository.java
+++ src/main/java/com/joebrooks/mapshotapi/repository/notice/NoticeRepository.java
@@ -5,5 +5,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeRepository extends JpaRepository<NoticeEntity, Long> {
-    List<NoticeEntity> findTop10ByIdLessThanEqualOrderByIdDesc(long id);
+    List<NoticeEntity> findTop10ByIdLessThanOrderByIdDesc(long id);
+
+    NoticeEntity findFirstByOrderByIdDesc();
+
 }
