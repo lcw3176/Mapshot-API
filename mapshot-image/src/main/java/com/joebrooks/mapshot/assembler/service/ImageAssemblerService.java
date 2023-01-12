c1f20838c004f344b5eb0703b4424bd3a65de782
lcw3176 <lcw3176@naver.com>
2023-01-12 10:49:44 +0900

    refactor(ImageAssemblerService): remove 함수 추가

--- mapshot-image/src/main/java/com/joebrooks/mapshot/assembler/service/ImageAssemblerService.java
+++ mapshot-image/src/main/java/com/joebrooks/mapshot/assembler/service/ImageAssemblerService.java
@@ -1,11 +1,9 @@
 package com.joebrooks.mapshot.assembler.service;
 
 import com.joebrooks.mapshot.assembler.model.ImageRequest;
-import com.joebrooks.mapshot.assembler.model.ImageResponse;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,10 +11,17 @@ import org.springframework.stereotype.Service;
 public class ImageAssemblerService {
 
     private final Queue<ImageRequest> imageRequestQueue = new ConcurrentLinkedQueue<>();
-    private final SimpMessagingTemplate messagingTemplate;
+
 
     public void addToQueue(ImageRequest taskRequest) {
         imageRequestQueue.add(taskRequest);
+    }
+
+    public void removeFromQueue(String sessionId) {
+        imageRequestQueue.stream()
+                .filter(i -> i.getSessionId().equals(sessionId))
+                .findAny()
+                .ifPresent(imageRequestQueue::remove);
     }
 
     public boolean isQueueEmpty() {
@@ -25,9 +30,5 @@ public class ImageAssemblerService {
 
     public ImageRequest pollFromQueue() {
         return imageRequestQueue.poll();
-    }
-
-    public void sendImageInfoToUser(ImageResponse response) {
-        messagingTemplate.convertAndSend("/assembler/result/" + response.getSessionId(), response);
     }
 }
