60d9193becdd15ae8de9da50f1fc66530ac745a4
lcw3176 <lcw3176@naver.com>
2023-01-12 10:51:42 +0900

    refactor(WaitListService, WaitListController): 컨트롤러 쪽으로 유저 통신 함수 이전

--- mapshot-image/src/main/java/com/joebrooks/mapshot/waitlist/service/WaitListService.java
+++ mapshot-image/src/main/java/com/joebrooks/mapshot/waitlist/service/WaitListService.java
@@ -1,50 +1,36 @@
 package com.joebrooks.mapshot.waitlist.service;
 
-import com.joebrooks.mapshot.waitlist.model.WaitListResponse;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.socket.messaging.SessionConnectedEvent;
-import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Service
 @RequiredArgsConstructor
 public class WaitListService {
 
     private final List<String> userSessionList = Collections.synchronizedList(new ArrayList<>());
-    private final SimpMessagingTemplate messagingTemplate;
+
 
     public int getPosition(String sessionId) {
         return userSessionList.indexOf(sessionId);
     }
 
-
-    @EventListener
-    private void onSessionConnectedEvent(SessionConnectedEvent event) {
-        StompHeaderAccessor sha = StompHeaderAccessor.wrap(event.getMessage());
-        userSessionList.add(sha.getSessionId());
+    public void addSession(String sessionId) {
+        userSessionList.add(sessionId);
     }
 
-    @EventListener
-    private void onSessionDisconnectEvent(SessionDisconnectEvent event) {
-        StompHeaderAccessor sha = StompHeaderAccessor.wrap(event.getMessage());
-        userSessionList.remove(sha.getSessionId());
-
-        for (int i = 0; i < userSessionList.size(); i++) {
-            sendWaitersCountToUser(
-                    WaitListResponse.builder()
-                            .sessionId(userSessionList.get(i))
-                            .index(i)
-                            .build());
-        }
+    public void removeSession(String sessionId) {
+        userSessionList.remove(sessionId);
     }
 
-    private void sendWaitersCountToUser(WaitListResponse waiter) {
-        messagingTemplate.convertAndSend("/wait-list/result", waiter);
+    public int getSessionsSize() {
+        return userSessionList.size();
     }
+
+    public String getSession(int index) {
+        return userSessionList.get(index);
+    }
+
 }
