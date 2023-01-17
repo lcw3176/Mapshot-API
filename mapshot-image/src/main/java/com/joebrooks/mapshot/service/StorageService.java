63b90d5ab28a88102e99d6b4c5b8fe270f368a28
lcw3176 <lcw3176@naver.com>
2023-01-17 15:39:43 +0900

    refactor(StorageService, StorageController): 패키지 위치 변경

---
+++ mapshot-image/src/main/java/com/joebrooks/mapshot/service/StorageService.java
@@ -0,0 +1,42 @@
+package com.joebrooks.mapshot.service;
+
+
+import com.joebrooks.mapshot.model.StorageInner;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StorageService {
+
+    private final Map<String, StorageInner> map = new ConcurrentHashMap<>();
+
+    public void add(StorageInner storageInner) {
+        map.put(storageInner.getUuid(), storageInner);
+    }
+
+    public byte[] getImage(String uuid) {
+        byte[] image = map.get(uuid).getImageByte();
+        map.remove(uuid);
+
+        return image;
+    }
+
+    public List<StorageInner> getAll() {
+        List<StorageInner> temp = new LinkedList<>();
+
+        for (String i : map.keySet()) {
+            temp.add(map.get(i));
+        }
+
+        return temp;
+    }
+
+    public void remove(String uuid) {
+        map.remove(uuid);
+    }
+}
