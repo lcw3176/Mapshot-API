df4b48302cda8154a21f10d0d16fc9d713653baa
lcw3176 <lcw3176@naver.com>
2023-01-17 15:38:49 +0900

    feat(mapshot-image): enum 클래스들 추가

---
+++ mapshot-image/src/main/java/com/joebrooks/mapshot/enums/CompanyType.java
@@ -0,0 +1,8 @@
+package com.joebrooks.mapshot.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum CompanyType {
+    kakao,
+}
---
+++ mapshot-image/src/main/java/com/joebrooks/mapshot/enums/ImageType.java
@@ -0,0 +1,8 @@
+package com.joebrooks.mapshot.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum ImageType {
+    JPEG
+}
