9d8d8c7a920fca1980cf419da77112ee0e8f6edd
lcw3176 <lcw3176@naver.com>
2023-01-21 17:39:13 +0900

    feat(JwtTokenProvider): 토큰 발급 클래스 추가

---
+++ mapshot-common/src/main/java/com/joebrooks/mapshot/auth/JwtTokenProvider.java
@@ -0,0 +1,37 @@
+package com.joebrooks.mapshot.auth;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.time.Duration;
+import java.util.Date;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class JwtTokenProvider {
+
+    // fixme 나중에 키 값 뺄것
+    private static final String JWT_SECRET = "iamtestkey";
+    public static final String HEADER_NAME = "test_header";
+
+    public static String generate() {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setExpiration(new Date(now.getTime() + Duration.ofSeconds(60).toMillis()))
+                .signWith(SignatureAlgorithm.HS512, JWT_SECRET)
+                .compact();
+    }
+
+
+    public static boolean isValid(String token) {
+        try {
+            Jwts.parser().setSigningKey(JWT_SECRET).parseClaimsJws(token);
+
+            return true;
+        } catch (Exception e) {
+
+            return false;
+        }
+    }
+
+}
