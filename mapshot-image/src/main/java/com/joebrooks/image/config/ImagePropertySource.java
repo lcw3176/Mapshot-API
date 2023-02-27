1d5b33f4415783b703545c368c41628d8fc657e0
lcw3176 <lcw3176@naver.com>
2023-02-27 22:42:19 +0900

    refactor: 패키지별 yml 주입 코드 추가

---
+++ mapshot-image/src/main/java/com/joebrooks/image/config/ImagePropertySource.java
@@ -0,0 +1,12 @@
+package com.joebrooks.image.config;
+
+import com.joebrooks.common.util.YamlPropertySourceFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ConfigurationProperties(prefix = "yaml")
+@PropertySource(value = "classpath:application-image.yml", factory = YamlPropertySourceFactory.class)
+public class ImagePropertySource {
+}
---
+++ mapshot-notice/src/main/java/com/joebrooks/notice/config/NoticePropertySource.java
@@ -0,0 +1,12 @@
+package com.joebrooks.notice.config;
+
+import com.joebrooks.common.util.YamlPropertySourceFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ConfigurationProperties(prefix = "yaml")
+@PropertySource(value = "classpath:application-notice.yml", factory = YamlPropertySourceFactory.class)
+public class NoticePropertySource {
+}
