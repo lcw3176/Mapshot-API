17106a47f0afc4402dc1b7c42ce8e411a8c7b458
lcw3176 <lcw3176@naver.com>
2023-02-28 23:07:56 +0900

    refactor: config 파일 패키지 상단에 위치하도록 변경

--- mapshot-image/src/main/java/com/joebrooks/image/ImageModuleConfiguration.java
+++ mapshot-image/src/main/java/com/joebrooks/image/ImageModuleConfiguration.java
@@ -1,4 +1,4 @@
-package com.joebrooks.notice.config;
+package com.joebrooks.image;
 
 import com.joebrooks.common.util.YamlPropertySourceFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ConfigurationProperties(prefix = "yaml")
-@PropertySource(value = "classpath:application-notice.yml", factory = YamlPropertySourceFactory.class)
-public class NoticePropertySource {
+@PropertySource(value = "classpath:application-image.yml", factory = YamlPropertySourceFactory.class)
+public class ImageModuleConfiguration {
+    
 }
--- mapshot-notice/src/main/java/com/joebrooks/notice/NoticeModuleConfiguration.java
+++ mapshot-notice/src/main/java/com/joebrooks/notice/NoticeModuleConfiguration.java
@@ -1,12 +1,14 @@
-package com.joebrooks.image.config;
+package com.joebrooks.notice;
 
 import com.joebrooks.common.util.YamlPropertySourceFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+
 @Configuration
 @ConfigurationProperties(prefix = "yaml")
-@PropertySource(value = "classpath:application-image.yml", factory = YamlPropertySourceFactory.class)
-public class ImagePropertySource {
+@PropertySource(value = "classpath:application-notice.yml", factory = YamlPropertySourceFactory.class)
+public class NoticeModuleConfiguration {
+
 }
