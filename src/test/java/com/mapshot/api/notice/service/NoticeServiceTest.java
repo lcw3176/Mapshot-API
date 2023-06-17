573c51a151a37260ae6e97856c779c5cb4350978
lcw3176 <lcw3176@naver.com>
2023-06-17 16:59:28 +0900

    test: notice 업데이트 테스트

--- src/test/java/com/mapshot/api/notice/service/NoticeServiceTest.java
+++ src/test/java/com/mapshot/api/notice/service/NoticeServiceTest.java
@@ -6,8 +6,6 @@ import com.mapshot.api.notice.enums.NoticeType;
 import com.mapshot.api.notice.model.NoticeDetailResponse;
 import com.mapshot.api.notice.model.NoticeRequest;
 import com.mapshot.api.notice.model.NoticeSummaryResponse;
-import com.mapshot.api.notice.repository.NoticeRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -27,12 +25,56 @@ class NoticeServiceTest {
     @Autowired
     private NoticeService noticeService;
 
-    @Autowired
-    private NoticeRepository noticeRepository;
+    @Test
+    void 저장_테스트() {
 
-    @AfterEach
-    void release() {
-        noticeRepository.deleteAll();
+        NoticeRequest request = NoticeRequest.builder()
+                .noticeType(NoticeType.FIX.toString())
+                .title("헬로")
+                .content("방가방가")
+                .build();
+
+        long id = noticeService.save(request);
+
+        long savedId = noticeService.getSinglePost(id).getId();
+
+        assertEquals(id, savedId);
+    }
+
+    @Test
+    void 업데이트_테스트() {
+
+        NoticeRequest request = NoticeRequest.builder()
+                .noticeType(NoticeType.UPDATE.toString())
+                .title("초기화")
+                .content("초기화")
+                .build();
+
+        long id = noticeService.save(request);
+
+        long updatedId = noticeService.update(NoticeRequest.builder()
+                .id(id)
+                .noticeType(NoticeType.FIX.toString())
+                .title("헬로")
+                .content("헬로")
+                .build());
+
+
+        assertEquals(id, updatedId);
+
+        String noticeType = noticeService.getSinglePost(updatedId).getNoticeType();
+        assertEquals(NoticeType.FIX.getKorean(), noticeType);
+    }
+
+
+    @Test
+    void 시작_id를_0으로_전송_시_가장_최근_게시글부터_10개_반환() {
+        int id = 0;
+
+        List<NoticeSummaryResponse> lst = noticeService.getMultiplePostsSummary(id);
+        assertThat(lst).hasSize(10)
+                .isSortedAccordingTo(Comparator.comparing(NoticeSummaryResponse::getId).reversed());
+
     }
 
 
@@ -77,32 +119,4 @@ class NoticeServiceTest {
     }
 
 
-    @Test
-    void 시작_id를_0으로_전송_시_가장_최근_게시글부터_10개_반환() {
-        int id = 0;
-
-        List<NoticeSummaryResponse> lst = noticeService.getMultiplePostsSummary(id);
-        assertThat(lst).hasSize(10)
-                .isSortedAccordingTo(Comparator.comparing(NoticeSummaryResponse::getId).reversed());
-
-    }
-
-
-    @Test
-    void 저장_테스트() {
-
-        NoticeRequest request = NoticeRequest.builder()
-                .noticeType(NoticeType.FIX.toString())
-                .title("헬로")
-                .content("방가방가")
-                .build();
-
-        long id = noticeService.save(request);
-
-        long savedId = noticeService.getSinglePost(id).getId();
-
-        assertEquals(id, savedId);
-    }
-
-    
 }
