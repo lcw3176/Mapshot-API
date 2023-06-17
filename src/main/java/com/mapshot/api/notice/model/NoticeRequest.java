d09a021ec8cbcae12bc8b752689e702499b7c2a8
lcw3176 <lcw3176@naver.com>
2023-06-17 14:59:23 +0900

    feat: noticeRequest 추가

---
+++ src/main/java/com/mapshot/api/notice/model/NoticeRequest.java
@@ -0,0 +1,27 @@
+package com.mapshot.api.notice.model;
+
+import com.mapshot.api.notice.entity.NoticeEntity;
+import com.mapshot.api.notice.enums.NoticeType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NoticeRequest {
+
+    private String noticeType;
+    private String title;
+    private String content;
+    
+    public NoticeEntity toEntity() {
+        return NoticeEntity.builder()
+                .noticeType(NoticeType.valueOf(noticeType))
+                .content(content)
+                .title(title)
+                .build();
+    }
+}
