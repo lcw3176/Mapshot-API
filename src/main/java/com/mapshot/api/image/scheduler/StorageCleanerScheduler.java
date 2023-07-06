944cf68cf7a7ae3519e2fc33501b924f74bb39fe
lcw3176 <lcw3176@naver.com>
2023-07-06 20:16:49 +0900

    refactor: 클리너 작동 시간 조절

--- src/main/java/com/mapshot/api/image/scheduler/StorageCleanerScheduler.java
+++ src/main/java/com/mapshot/api/image/scheduler/StorageCleanerScheduler.java
@@ -3,11 +3,12 @@ package com.mapshot.api.image.scheduler;
 
 import com.mapshot.api.image.model.StorageInner;
 import com.mapshot.api.image.service.StorageService;
-import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 @EnableScheduling
@@ -16,12 +17,12 @@ public class StorageCleanerScheduler {
 
     private final StorageService storageService;
 
-    @Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0 * * * * *")
     public void clean() {
         LocalDateTime nowTime = LocalDateTime.now();
 
         for (StorageInner i : storageService.getAll()) {
-            if (i.getCreatedAt().plusMinutes(30).isBefore(nowTime)) {
+            if (i.getCreatedAt().plusMinutes(5).isBefore(nowTime)) {
                 storageService.remove(i.getUuid());
             }
         }
