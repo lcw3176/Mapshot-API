7ed45091f0fc0bdd1750fb3b9848b55f4f412d01
lcw3176 <lcw3176@naver.com>
2023-08-28 23:55:18 +0900

    ContentControllerTest 케이스 추가

--- src/test/java/com/mapshot/api/board/content/controller/ContentControllerTest.java
+++ src/test/java/com/mapshot/api/board/content/controller/ContentControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -21,8 +22,11 @@ import java.util.Comparator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -99,7 +103,8 @@ class ContentControllerTest extends SlackMockExtension {
                 .build();
 
         long id = contentService.save(request);
-        
+
+
         mockMvc.perform(
                         RestDocumentationRequestBuilders.get(BASE_URL + "/{id}", id))
                 .andExpect(status().isOk())
@@ -112,5 +117,38 @@ class ContentControllerTest extends SlackMockExtension {
                                                 "게시글 id")
                         )));
 
+    }
+
+    @Test
+    void 게시글_등록_테스트() throws Exception {
+        ContentRequest request = ContentRequest
+                .builder()
+                .title("등록 타이틀")
+                .content("등록 내용")
+                .nickname("등록 닉네임")
+                .build();
+
+        String bodyContent = mapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                        RestDocumentationRequestBuilders.post(BASE_URL + "/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(bodyContent))
+                .andExpect(status().isOk())
+                .andDo(document("board/content/register",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("title")
+                                        .description("게시글 제목"),
+                                fieldWithPath("content")
+                                        .description("게시글 내용"),
+                                fieldWithPath("nickname")
+                                        .description("작성자 닉네임")
+                        )));
+
+        List<ContentListResponse> contents = contentService.getContentList(1);
+
+        assertEquals(contents.get(0).getTitle(), request.getTitle());
     }
 }
