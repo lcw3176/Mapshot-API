ceee4d79a80454c2af9851f4019ee7585fa15863
lcw3176 <lcw3176@naver.com>
2024-03-16 22:02:53 +0900

    토큰 프로세서 추가

---
+++ src/test/java/com/mapshot/api/auth/validation/TokenProcessorTest.java
@@ -0,0 +1,40 @@
+package com.mapshot.api.auth.validation;
+
+import com.mapshot.api.common.exception.ApiException;
+import com.mapshot.api.common.exception.status.ErrorCode;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+@SpringBootTest
+class TokenProcessorTest {
+
+    @Autowired
+    private TokenProcessor tokenProcessor;
+
+    private final String key = "sdfsefsdfsdf";
+
+    @Test
+    void 토큰_생성_테스트() {
+
+        String token = tokenProcessor.makeToken(60, key);
+
+        assertThatNoException().isThrownBy(() -> {
+            tokenProcessor.isValid(key, token);
+        });
+    }
+
+    @Test
+    void 유효하지_않은_토큰이면_예외_발생() {
+        String token = "hellohello";
+
+        assertThatThrownBy(() -> tokenProcessor.isValid(key, token))
+                .isInstanceOf(ApiException.class)
+                .hasMessage(ErrorCode.NOT_VALID_TOKEN.getMessage());
+    }
+
+}
