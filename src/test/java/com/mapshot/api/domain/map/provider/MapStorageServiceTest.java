7ce091686f7308b032e5459831f3225edb84041c
lcw3176 <lcw3176@naver.com>
2024-03-17 22:13:08 +0900

    테스트 수정

--- src/test/java/com/mapshot/api/domain/map/provider/MapStorageServiceTest.java
+++ src/test/java/com/mapshot/api/domain/map/provider/MapStorageServiceTest.java
@@ -5,6 +5,8 @@ import com.mapshot.api.presentation.map.provider.model.StorageRequest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -13,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MapStorageServiceTest {
 
     private final MapStorageService mapStorageService = new MapStorageService();
+    private static final String BASE64_IMAGE = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAEElEQVR4nGIy7foHCAAA//8CvQHAzMBtMgAAAABJRU5ErkJggg==";
 
     @AfterEach
     void reset() {
@@ -26,8 +29,9 @@ class MapStorageServiceTest {
         int size = 100;
 
         for (int i = 0; i < size; i++) {
-            new Thread(() -> mapStorageService.add(StorageRequest.builder()
-                    .base64EncodedImage("sdgdsgsdg")
+            new Thread(() -> mapStorageService.add(StorageRequest
+                    .builder()
+                    .base64EncodedImage(BASE64_IMAGE)
                     .uuid(UUID.randomUUID().toString())
                     .build())).start();
         }
@@ -48,15 +52,14 @@ class MapStorageServiceTest {
     @Test
     void 이미지는_반환과_동시에_삭제됨() {
         String uuid = UUID.randomUUID().toString();
-        String imageByte = "I am Virtual Image";
 
         mapStorageService.add(StorageRequest.builder()
-                .base64EncodedImage(imageByte)
+                .base64EncodedImage(BASE64_IMAGE)
                 .uuid(uuid)
                 .build());
         byte[] image = mapStorageService.getImage(uuid);
 
-        assertEquals(image, imageByte.getBytes());
+        assertEquals(Arrays.toString(image), Arrays.toString(Base64.getDecoder().decode(BASE64_IMAGE)));
         assertThatThrownBy(() -> mapStorageService.getImage(uuid))
                 .isInstanceOf(NullPointerException.class);
     }
