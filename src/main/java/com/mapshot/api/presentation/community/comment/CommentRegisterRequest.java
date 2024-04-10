b2e0823de8e7f07c9a02c46173b5e25b80cba779
lcw3176 <lcw3176@naver.com>
2024-04-10 19:24:53 +0900

    밸리데이션 변경

--- src/main/java/com/mapshot/api/presentation/community/comment/CommentRegisterRequest.java
+++ src/main/java/com/mapshot/api/presentation/community/comment/CommentRegisterRequest.java
@@ -2,6 +2,7 @@ package com.mapshot.api.presentation.community.comment;
 
 import com.mapshot.api.domain.community.comment.CommentConst;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,7 +15,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CommentRegisterRequest {
 
-    @NotBlank
+    @PositiveOrZero
     private Long postId;
 
     @NotBlank
