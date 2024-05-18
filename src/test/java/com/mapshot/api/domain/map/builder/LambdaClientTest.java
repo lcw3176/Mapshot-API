025a4642cec220f53840bf6300a0c34ddad57533
lcw3176 <lcw3176@naver.com>
2024-05-18 16:42:23 +0900

    호스트 데이터 포맷 변경

--- src/test/java/com/mapshot/api/domain/map/builder/LambdaClientTest.java
+++ src/test/java/com/mapshot/api/domain/map/builder/LambdaClientTest.java
@@ -1,8 +1,8 @@
 package com.mapshot.api.domain.map.builder;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
@@ -17,7 +17,7 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 class LambdaClientTest {
 
-    @Autowired
+    @MockBean
     private LambdaClient lambdaClient;
 
     @Test
