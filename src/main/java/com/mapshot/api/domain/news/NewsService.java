244a43b4b3cb0b7cc465254d5df4d09246d29b8b
lcw3176 <lcw3176@naver.com>
2024-05-20 13:53:33 +0900

    html 태그 제작 함수 분리, 메타 이미지 관련 코드 추가

--- src/main/java/com/mapshot/api/domain/news/NewsService.java
+++ src/main/java/com/mapshot/api/domain/news/NewsService.java
@@ -7,13 +7,19 @@ import com.mapshot.api.domain.news.client.naver.NaverClient;
 import com.mapshot.api.domain.news.client.naver.NaverNewsDto;
 import com.mapshot.api.domain.news.client.naver.NaverNewsResponse;
 import lombok.RequiredArgsConstructor;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -53,10 +59,15 @@ public class NewsService {
             String lineSpace = wrapHtmlTag("", "p");
             String title = wrapHtmlTag(index++ + ". " + detailNews.getTitle(), "h3");
             String description = wrapHtmlTag(detailNews.getDescription(), "p");
-            String link = wrapHtmlTag(detailNews.getOriginallink(), "a");
+            String link = wrapHtmlTag(detailNews.getOriginallink(), "a", Map.of("href", detailNews.getOriginallink()));
+
+            String imageLink = getMetaImage(detailNews.getOriginallink());
+            imageLink = wrapHtmlTag("", "img", Map.of("src", imageLink));
 
             contents.append(lineSpace);
             contents.append(title);
+            contents.append(lineSpace);
+            contents.append(imageLink);
             contents.append(lineSpace);
             contents.append(description);
             contents.append(lineSpace);
@@ -71,19 +82,9 @@ public class NewsService {
     private String wrapHtmlTag(String content, String tag) {
         StringBuilder sb = new StringBuilder();
 
-        if (tag.equals("a")) {
-            sb.append("<");
-            sb.append(tag);
-            sb.append(" ");
-            sb.append("href=");
-            sb.append(content);
-            sb.append(">");
-        } else {
-            sb.append("<");
-            sb.append(tag);
-            sb.append(">");
-        }
-
+        sb.append("<");
+        sb.append(tag);
+        sb.append(">");
         sb.append(content);
 
         sb.append("</");
@@ -91,6 +92,53 @@ public class NewsService {
         sb.append(">");
 
         return sb.toString();
+    }
+
+
+    private String wrapHtmlTag(String content, String tag, Map<String, String> attr) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<");
+        sb.append(tag);
+
+        for (String key : attr.keySet()) {
+            sb.append(" ");
+            sb.append(key);
+            sb.append("=");
+            sb.append(attr.get(key));
+        }
+
+        sb.append(">");
+        sb.append(content);
+
+        sb.append("</");
+        sb.append(tag);
+        sb.append(">");
+
+        return sb.toString();
+    }
+
+
+    private String getMetaImage(String url) {
+        Document document = null;
+
+        try {
+            document = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Elements meta = document.getElementsByTag("meta");
+
+        for (Element i : meta) {
+            String prop = i.attr("property");
+
+            if (prop.equals("og:image")) {
+                return i.attr("content");
+            }
+        }
+
+        return "";
     }
 
 }
