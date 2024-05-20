a41a2ebfc4cf1c5db0854319b1a7dae56c24aed2
lcw3176 <lcw3176@naver.com>
2024-05-20 20:37:45 +0900

    시간 수정

--- src/main/java/com/mapshot/api/domain/news/NewsBotScheduler.java
+++ src/main/java/com/mapshot/api/domain/news/NewsBotScheduler.java
@@ -12,7 +12,7 @@ public class NewsBotScheduler {
     private final NewsService newsService;
 
     // 매일 오후 9시에 당일 국토교통부 보도자료 업데이트
-    @Scheduled(cron = "0 35 20 * * *")
+    @Scheduled(cron = "0 0 21 * * *")
     public void update() {
         newsService.updateNewsLetter();
     }
