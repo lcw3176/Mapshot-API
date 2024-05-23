c1e8f75e66f76a32acabd592561cd4e68eddab0e
Chanwoo Lee <lcw3176@naver.com>
2024-05-23 21:20:19 +0900

    임시 어드민 뉴스 컨트롤러 (#57)
    * 임시 어드민 뉴스 컨트롤러
    
    * 밸리데이트 어노테이션 제거

---
+++ src/main/java/com/mapshot/api/presentation/admin/AdminNewsController.java
@@ -0,0 +1,29 @@
+package com.mapshot.api.presentation.admin;
+
+import com.mapshot.api.domain.news.NewsService;
+import com.mapshot.api.infra.auth.annotation.PreAuth;
+import com.mapshot.api.infra.auth.enums.Accessible;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/news")
+@CrossOrigin(originPatterns = {"https://*.kmapshot.com", "https://kmapshot.com"})
+public class AdminNewsController {
+
+    private final NewsService newsService;
+
+
+    @PreAuth(Accessible.ADMIN)
+    @GetMapping("/update")
+    public ResponseEntity<Void> updateNewsLetter() {
+        newsService.updateNewsLetter();
+
+        return ResponseEntity.ok().build();
+    }
+}
