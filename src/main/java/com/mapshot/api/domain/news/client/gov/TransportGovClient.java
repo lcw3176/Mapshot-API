dcffb013e42ae20a6543045f2f36b322c882f504
lcw3176 <lcw3176@naver.com>
2024-06-17 22:22:34 +0900

    jsoup 설정 추가

--- src/main/java/com/mapshot/api/domain/news/client/gov/TransportGovClient.java
+++ src/main/java/com/mapshot/api/domain/news/client/gov/TransportGovClient.java
@@ -29,6 +29,7 @@ public class TransportGovClient {
 
             try {
                 doc = Jsoup.connect(path)
+                        .timeout(0)
                         .ignoreContentType(true)
                         .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
                         .followRedirects(true)
