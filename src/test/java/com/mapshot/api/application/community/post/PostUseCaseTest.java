41b9993bec8e30702e478148bca0b279cc9d8bf3
lcw3176 <lcw3176@naver.com>
2024-06-18 08:51:03 +0900

    PostUseCase 테스트 추가

--- src/test/java/com/mapshot/api/application/community/post/PostUseCaseTest.java
+++ src/test/java/com/mapshot/api/application/community/post/PostUseCaseTest.java
@@ -4,6 +4,8 @@ import com.mapshot.api.domain.community.comment.CommentEntity;
 import com.mapshot.api.domain.community.comment.CommentRepository;
 import com.mapshot.api.domain.community.post.PostEntity;
 import com.mapshot.api.domain.community.post.PostRepository;
+import com.mapshot.api.infra.exception.ApiException;
+import com.mapshot.api.infra.exception.status.ErrorCode;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,8 +14,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 class PostUseCaseTest {
@@ -79,13 +83,28 @@ class PostUseCaseTest {
     }
 
     @Test
+    void 존재하지_않는_게시글을_가져오면_예와가_발생한다() {
+        assertThatThrownBy(() -> postUseCase.getPost(-1))
+                .isInstanceOf(ApiException.class)
+                .hasMessageContaining(ErrorCode.NO_SUCH_POST.getMessage());
+    }
+
+    @Test
     void 게시글_목록을_가져온다() {
         PostListResponse response = postUseCase.getPostList(1);
 
         assertEquals(response.getTotalPage(), 1);
         assertEquals(response.getPosts().size(), 10);
-
     }
+
+    @Test
+    void _0이하의_페이지_조회시_첫번째_페이지를_반환한다() {
+        PostListResponse response = postUseCase.getPostList(-1);
+
+        assertEquals(response.getTotalPage(), 1);
+        assertEquals(response.getPosts().size(), 10);
+    }
+    
 
     @Test
     void 게시글을_저장한다() {
@@ -99,11 +118,31 @@ class PostUseCaseTest {
     }
 
     @Test
+    void 비밀번호는_저장시_암호화된다() {
+        postUseCase.save("writer", "content", "title", "password");
+
+        PostEntity post = postRepository.findFirstByOrderByIdDesc();
+
+        assertNotEquals(post.getPassword(), "password");
+    }
+
+
+    @Test
     void 게시글을_삭제한다() {
         postUseCase.save("writer", "content", "title", "password");
 
         PostEntity post = postRepository.findFirstByOrderByIdDesc();
 
-        assertDoesNotThrow(() -> postUseCase.deleteIfOwner(post.getId(), "password"));
+        assertThatNoException().isThrownBy(() -> postUseCase.deleteIfOwner(post.getId(), "password"));
+    }
+
+
+    @Test
+    void 작성자가_아니면_게시글을_삭제할수_없다() {
+        PostEntity post = postRepository.findFirstByOrderByIdDesc();
+
+        assertThatThrownBy(() -> postUseCase.deleteIfOwner(post.getId(), "wrong password"))
+                .isInstanceOf(ApiException.class)
+                .hasMessageContaining(ErrorCode.NOT_VALID_PASSWORD.getMessage());
     }
 }
