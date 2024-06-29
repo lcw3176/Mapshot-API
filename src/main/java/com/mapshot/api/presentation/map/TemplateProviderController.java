1fe5aed758a2a9b391463ef9bd9a94b6ae8f3cce
lcw3176 <lcw3176@naver.com>
2024-06-29 20:52:26 +0900

    인증 서버만 접근할수 있게 변경

--- src/main/java/com/mapshot/api/presentation/map/TemplateProviderController.java
+++ src/main/java/com/mapshot/api/presentation/map/TemplateProviderController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/image/template")
 public class TemplateProviderController {
 
-    @PreAuth(Accessible.EVERYONE)
+    @PreAuth(Accessible.FRIENDLY_SERVER)
     @GetMapping("/kakao")
     public String getKakaoMap(@ModelAttribute MapBuildRequest mapRequest, Model model) {
         model.addAttribute("mapRequest", mapRequest);
@@ -20,7 +20,7 @@ public class TemplateProviderController {
         return "map/kakao";
     }
 
-    @PreAuth(Accessible.EVERYONE)
+    @PreAuth(Accessible.FRIENDLY_SERVER)
     @GetMapping("/google")
     public String getGoogleMap(@ModelAttribute MapBuildRequest mapRequest, Model model) {
         model.addAttribute("mapRequest", mapRequest);
