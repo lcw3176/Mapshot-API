c340be3105dc5dbce575dce16a57c88045136e5a
lcw3176 <lcw3176@naver.com>
2024-07-07 16:32:35 +0900

    코드 정리

--- src/test/java/com/mapshot/api/presentation/map/ImageBuildRequestControllerTest.java
+++ src/test/java/com/mapshot/api/presentation/map/ImageBuildRequestControllerTest.java
@@ -1,6 +1,5 @@
 package com.mapshot.api.presentation.map;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -19,9 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureRestDocs
 @AutoConfigureMockMvc
 class ImageBuildRequestControllerTest {
-
-    @Autowired
-    ObjectMapper mapper;
+    
 
     @Autowired
     MockMvc mockMvc;
