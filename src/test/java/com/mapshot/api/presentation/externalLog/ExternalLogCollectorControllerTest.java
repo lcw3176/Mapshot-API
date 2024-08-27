21703a01976668f4a8b6b7f7c4a10f59941d4778
lcw3176 <lcw3176@naver.com>
2024-08-27 15:28:54 +0900

    테스트 추가

---
+++ src/test/java/com/mapshot/api/presentation/externalLog/ExternalLogCollectorControllerTest.java
@@ -0,0 +1,59 @@
+package com.mapshot.api.presentation.externalLog;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mapshot.api.infra.auth.ServerValidation;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
+class ExternalLogCollectorControllerTest {
+
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Autowired
+    private ServerValidation validation;
+
+    @Test
+    void 로그_수집_요청() throws Exception {
+
+        LogRequest request = LogRequest.builder()
+                .roll("testRoll")
+                .stackTrace("testStackTrace")
+                .build();
+
+
+        mockMvc.perform(post("/log")
+                        .headers(validation.makeHeader())
+                        .content(mapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("log",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("roll").description("에러가 발생한 서버의 역할, 기능"),
+                                fieldWithPath("stackTrace").description("에러 스택트레이스")
+                        )))
+                .andReturn();
+
+    }
+}
