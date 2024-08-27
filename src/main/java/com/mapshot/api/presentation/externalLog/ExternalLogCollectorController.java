64924918a1a26e6c15fc7841c76f3d668b75fcaf
Chanwoo Lee <lcw3176@naver.com>
2024-08-27 15:32:40 +0900

    Feat/로그 수집 (#76)
    * 슬랙 메세지 함수 추가
    
    * 외부 로그 수집 기능 추가
    
    * 테스트 추가
    
    * 어노테이션 추가

---
+++ src/main/java/com/mapshot/api/presentation/externalLog/ExternalLogCollectorController.java
@@ -0,0 +1,24 @@
+package com.mapshot.api.presentation.externalLog;
+
+import com.mapshot.api.application.log.LogUseCase;
+import com.mapshot.api.infra.auth.annotation.PreAuth;
+import com.mapshot.api.infra.auth.enums.Accessible;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/log")
+@RequiredArgsConstructor
+public class ExternalLogCollectorController {
+
+    private final LogUseCase logUseCase;
+
+    @PreAuth(Accessible.FRIENDLY_SERVER)
+    @PostMapping
+    public void collectExternalLog(@RequestBody LogRequest request) {
+        logUseCase.collectLog(request.getRoll(), request.getStackTrace());
+    }
+}
