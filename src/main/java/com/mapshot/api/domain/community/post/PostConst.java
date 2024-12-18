4657dc3c22e06be3a577a10667494dcab067b1f1
lcw3176 <lcw3176@naver.com>
2024-12-18 22:04:59 +0900

    길이 조절

--- src/main/java/com/mapshot/api/domain/community/post/PostConst.java
+++ src/main/java/com/mapshot/api/domain/community/post/PostConst.java
@@ -2,6 +2,6 @@ package com.mapshot.api.domain.community.post;
 
 public class PostConst {
 
-    public static final int MAX_TITLE_LENGTH = 100;
-    public static final int MAX_CONTENT_LENGTH = 3000;
+    public static final int MAX_TITLE_LENGTH = 500;
+    public static final int MAX_CONTENT_LENGTH = 10000;
 }
