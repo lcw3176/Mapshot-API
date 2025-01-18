57fffdb4bd7c602c4cb7d658d1b4fda9c300c9a5
Chanwoo Lee <lcw3176@naver.com>
2025-01-18 21:05:05 +0900

    Feat/opentelemetry (#95)
    * 메모리가 버티려나??
    
    * 실험중
    
    * 테스트
    
    * 메모리 부족할듯
    
    * 다른 템플릿
    
    * 템플릿 제거
    
    * 템플릿 수정
    
    * 마무리
    
    * 마무리

---
+++ src/main/java/com/mapshot/api/infra/monitor/PrometheusConfiguration.java
@@ -0,0 +1,13 @@
+package com.mapshot.api.infra.monitor;
+
+import io.prometheus.client.exemplars.tracer.otel_agent.OpenTelemetryAgentSpanContextSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PrometheusConfiguration {
+    @Bean
+    public OpenTelemetryAgentSpanContextSupplier openTelemetryAgentSpanContextSupplier() {
+        return new OpenTelemetryAgentSpanContextSupplier();
+    }
+}
