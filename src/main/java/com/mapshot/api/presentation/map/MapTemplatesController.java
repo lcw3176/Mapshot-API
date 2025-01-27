b11de013eb75f78215c8caa652cbcb0b4c8d2962
lcw3176 <lcw3176@naver.com>
2025-01-28 01:11:12 +0900

    request 모델 정의

--- src/main/java/com/mapshot/api/presentation/map/MapTemplatesController.java
+++ src/main/java/com/mapshot/api/presentation/map/MapTemplatesController.java
@@ -1,9 +1,15 @@
 package com.mapshot.api.presentation.map;
 
+import com.mapshot.api.presentation.map.model.GoogleTemplateRequest;
+import com.mapshot.api.presentation.map.model.KakaoTemplateRequest;
+import com.mapshot.api.presentation.map.model.NaverTemplateRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,8 +17,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/map")
 public class MapTemplatesController {
 
-    @GetMapping("/google")
-    public String googleTemplate(Model model){
+    @PostMapping("/google")
+    public String googleTemplate(Model model, @Valid @RequestBody GoogleTemplateRequest request){
         return "google";
+    }
+
+    @PostMapping("/kakao")
+    public String kakaoTemplate(Model model, @Valid @RequestBody KakaoTemplateRequest request){
+        return "kakao";
+    }
+
+    @PostMapping("/naver")
+    public String naverTemplate(Model model, @Valid @RequestBody NaverTemplateRequest request){
+        return "naver";
     }
 }
---
+++ src/main/java/com/mapshot/api/presentation/map/model/NaverTemplateRequest.java
@@ -0,0 +1,32 @@
+package com.mapshot.api.presentation.map.model;
+
+import com.mapshot.api.domain.community.comment.CommentConst;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NaverTemplateRequest {
+
+    @NotBlank
+    private float lat;
+
+    @NotBlank
+    private float lng;
+
+    @NotBlank
+    @Positive
+    private int level;
+
+    @NotBlank
+    private String type;
+
+}
