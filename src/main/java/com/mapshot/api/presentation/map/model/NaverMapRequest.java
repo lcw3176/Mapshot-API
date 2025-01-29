469ddb8dd558ef11e6e3757e9bfab668a7a388d3
Chanwoo Lee <chanwoo3176@naver.com>
2025-01-29 23:03:09 +0900

    생성자 롬복 변경

--- src/main/java/com/mapshot/api/presentation/map/model/NaverMapRequest.java
+++ src/main/java/com/mapshot/api/presentation/map/model/NaverMapRequest.java
@@ -3,10 +3,8 @@ package com.mapshot.api.presentation.map.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class NaverMapRequest extends MapRequest {
