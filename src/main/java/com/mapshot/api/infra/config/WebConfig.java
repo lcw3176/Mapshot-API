a3f610d97e6a2128114de418a7dbe7ef4f392ad6
lcw3176 <lcw3176@naver.com>
2025-10-14 23:16:15 +0900

    webflux 의존성 제거

--- src/main/java/com/mapshot/api/infra/config/WebConfig.java
+++ src/main/java/com/mapshot/api/infra/config/WebConfig.java
@@ -2,10 +2,10 @@ package com.mapshot.api.infra.config;
 
 import com.mapshot.api.infra.interceptor.AuthInterceptor;
 import com.mapshot.api.infra.interceptor.RateLimitInterceptor;
-import io.netty.handler.codec.http.HttpMethod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
