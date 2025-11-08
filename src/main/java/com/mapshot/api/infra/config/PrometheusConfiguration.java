4391cc767583b8640a4f90be4cef4919b7cdd073
lcw3176 <lcw3176@naver.com>
2025-11-09 02:26:20 +0900

    센트리 센트리

--- src/main/java/com/mapshot/api/infra/config/PrometheusConfiguration.java
+++ src/main/java/com/mapshot/api/infra/config/PrometheusConfiguration.java
@@ -1,13 +1,14 @@
-package com.mapshot.api.infra.config;
-
-import io.prometheus.client.exemplars.tracer.otel_agent.OpenTelemetryAgentSpanContextSupplier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class PrometheusConfiguration {
-    @Bean
-    public OpenTelemetryAgentSpanContextSupplier openTelemetryAgentSpanContextSupplier() {
-        return new OpenTelemetryAgentSpanContextSupplier();
-    }
-}
+//package com.mapshot.api.infra.config;
+//
+//import io.prometheus.client.exemplars.tracer.otel_agent.OpenTelemetryAgentSpanContextSupplier;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class PrometheusConfiguration {
+//    @Bean
+//    public OpenTelemetryAgentSpanContextSupplier openTelemetryAgentSpanContextSupplier() {
+//        return new OpenTelemetryAgentSpanContextSupplier();
+//    }
+//}
+// 센트리로 몰빵
