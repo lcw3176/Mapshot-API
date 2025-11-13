5851dc0b52a7fc60bf0fc5d4ce2a2f4d64644588
lcw3176 <lcw3176@naver.com>
2025-11-13 21:48:55 +0900

    관련 리소스 정리

--- src/main/java/com/mapshot/api/infra/config/WebConfig.java
+++ src/main/java/com/mapshot/api/infra/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.mapshot.api.infra.config;
 import com.mapshot.api.infra.interceptor.AuthInterceptor;
 import com.mapshot.api.infra.interceptor.RateLimitInterceptor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -16,8 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor authInterceptor;
     private final RateLimitInterceptor rateLimitInterceptor;
-    @Value("${jwt.admin.header}")
-    public String ADMIN_HEADER_NAME;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -38,13 +35,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/admin/**")
-                .exposedHeaders(ADMIN_HEADER_NAME)
-                .allowedOrigins("https://www.kmapshot.com", "https://kmapshot.com", "https://dev.kmapshot.com", "https://admin.kmapshot.com")
-                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.OPTIONS.name())
-                .allowedHeaders("*")
-                .allowCredentials(true);
-
         registry.addMapping("/**")
                 .allowedOrigins("https://www.kmapshot.com", "https://kmapshot.com", "https://dev.kmapshot.com", "https://admin.kmapshot.com")
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.OPTIONS.name())
